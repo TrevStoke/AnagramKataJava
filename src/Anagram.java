fc590855e98c44496b06898ff88c3b9499db4555
Trevor Adams <trevor@trevweb.me.uk>
2012-09-17 21:10:13 +0100

    Anagram with Java, using Mac OS X built in dictionary. Replace with your own dictionary.

--- src/Anagram.java
+++ src/Anagram.java
@@ -12,7 +12,7 @@ import java.util.*;
 public class Anagram {
     public static void main(String args[]){
 
-        String check = "silent";
+        String check = "tinsel";
         String checkSorted = sortString(check);
         StringBuffer results = new StringBuffer(check);
 
